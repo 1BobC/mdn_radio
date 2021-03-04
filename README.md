96b1fb336e43aa2c5d859f09d3b055ea8efa9d99
Bob.C <bob.cocksedge@gmail.com>
2021-03-04 16:25:27 +0000

    First exercise works ok without style First node exercise ok

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # mdn_radio
 MDN exercises on radio buttons and node list for values
 "Create files html js and CSS Add code for MDN first radio exercise"
+"Complete radio buttons exercise with fault matching CSS code to Firefox native style will do node-list exercise next" 
+"First exercise works ok without style First node exercise ok"  
+
+Need to track this statement down for undestanding return property: 
+If the underlying element collection contains radio buttons, 
+the RadioNodeList.value property represents the checked radio button. On retrieving the 
+value property, the value of the currently checked radio button is returned as a string. 
+If the collection does not contain any radio buttons or none of the radio buttons in the collection 
+is in checked state, the empty string is returned. On setting the value property, the first 
+radio button input element whose value property is equal to the new value will be set to checked.
--- jscript.js
+++ jscript.js
@@ -1,12 +1,23 @@
-var form= document.querySelector("form");
-var log= document.querySelector("#log");
+//first example works ok wthout CSS!!
+// var form= document.querySelector("form");
+// var log= document.querySelector("#log");
 
-form.addEventListener("submit", function(event){
-    var data= new FormData(form);
-    var output= "";
-    for(const entry of data) {
-        output= output + entry[0] + "=" + entry[1] + "\r";
-    };
-    log.innerText= output;
-    event.preventDefault();
-}, false);
+// form.addEventListener("submit", function(event){
+//     var data= new FormData(form);
+//     var output= "";
+//     for(const entry of data) {
+//         output= output + entry[0] + "=" + entry[1] + "\r";
+//     };
+//     log.innerText= output;
+//     event.preventDefault();
+// }, false);
+
+// second example
+// Get the form
+const form = document.forms[0];
+
+// Get the form's radio buttons
+const radios = form.elements['color'];
+
+// Choose the "red" option
+radios.value = 'red';
